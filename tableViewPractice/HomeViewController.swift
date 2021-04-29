99a1d5ea1d1551414db979dcbe3e7d6001fa4c94
Ellen Yang <yange@oregonstate.edu>
2021-04-28 21:35:16 -0700

    Initial Commit

---
+++ tableViewPractice/HomeViewController.swift
@@ -0,0 +1,43 @@
+//
+//  HomeViewController.swift
+//  tableViewPractice
+//
+//  Created by Ellen Yang on 4/28/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UITableViewDataSource, UIWebViewDelegate, UITableViewDelegate {
+
+   @IBOutlet weak var tableView: UITableView!
+   
+   override func viewDidLoad() {
+        super.viewDidLoad()
+
+      tableView.delegate = self
+      tableView.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+
+   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return 5
+   }
+
+   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = UITableViewCell()
+      cell.textLabel?.text = "This is row \(indexPath.row)"
+
+      return cell
+   }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ tableViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableViewPractice
+//
+//  Created by Ellen Yang on 4/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
